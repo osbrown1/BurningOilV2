70ff09cc518ebb9dc462d7f9647e273d96a91d7d
Noah Atkinson <noahatkinson1.1@gmail.com>
2023-04-05 20:32:52 -0700

    add health text and apples heal you

--- Assets/Scripts/ItemCollector.cs
+++ Assets/Scripts/ItemCollector.cs
@@ -6,16 +6,29 @@ using UnityEngine.UI;
 public class ItemCollector : MonoBehaviour
 {
     private int apples = 0;
+    
 
     [SerializeField] private Text applesText;
+    [SerializeField] private PlayerHealth playerHealth;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (collision == null || collision.gameObject == null)
+        {
+            return;
+        }
         if (collision.gameObject.CompareTag("Apple"))
         {
+            if(!collision.gameObject.GetComponent<SpriteRenderer>().enabled)
+            {
+                return;
+            }
             Destroy(collision.gameObject);
             apples++;
             applesText.text = "Apples: " + apples;
+            playerHealth.Heal(10);
         }
     }
+
+
 }
